16f85117b2d7f6dfe1f6218ef9c1e0f2947fe47c
gerenook <8882732+gerenook@users.noreply.github.com>
2019-02-27 21:40:26 +0100

    clickable hyperlinks in articles

--- lib/screens/article/article_body.dart
+++ lib/screens/article/article_body.dart
@@ -14,7 +14,6 @@ class ArticleBody extends StatefulWidget {
 class ArticleBodyState extends State<ArticleBody> {
   @override
   Widget build(BuildContext context) {
-    // TODO hyperlinks, quoted text, selection, ...
     final TextStyle textStyle = Theme.of(context).textTheme.body1.copyWith(height: 1.15);
     return RichText(
       text: TextSpan(
@@ -22,34 +21,32 @@ class ArticleBodyState extends State<ArticleBody> {
         children: _buildTextSpans(context),
       ),
     );
-    // return Text(
-    //   widget.text,
-    //   style: textStyle,
-    // );
   }
 
   List<TextSpan> _buildTextSpans(BuildContext context) {
+    // TODO quoted text, selection, ...
     List<TextSpan> textSpans = [];
     Match prevMatch;
+    // URI regex
     RegExp regex = RegExp(r'(https?|ftp)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?');
     for (Match match in regex.allMatches(widget.text)) {
-      int textStart = (prevMatch?.end ?? -1) + 1;
-      textSpans.add(
-        TextSpan(
-          text: widget.text.substring(textStart, match.start)
-        )
-      );
+      // Add raw text until match
+      int textStart = prevMatch?.end ?? 0;
+      textSpans.add(TextSpan(text: widget.text.substring(textStart, match.start)));
+      // Add clickalbe uri
       String uri = match.group(0);
-      textSpans.add(
-        LinkTextSpan(
-          text: uri,
-          uri: uri,
-          context: context,
-        )
-      );
+      textSpans.add(LinkTextSpan(
+        text: uri,
+        uri: uri,
+        context: context,
+      ));
       prevMatch = match;
     }
-    // TODO trailing text block
+    // Add raw text after last match
+    int textStart = prevMatch?.end ?? 0;
+    if (textStart <= widget.text.length) {
+      textSpans.add(TextSpan(text: widget.text.substring(textStart, widget.text.length)));
+    }
     return textSpans;
   }
 }
