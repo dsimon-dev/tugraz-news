0b8bdf6a98e7bba866f2c5b758494cbf1f19a7fc
gerenook <8882732+gerenook@users.noreply.github.com>
2019-03-01 17:25:25 +0100

    grey out quoted text

--- lib/screens/article/article_body.dart
+++ lib/screens/article/article_body.dart
@@ -12,6 +12,8 @@ class ArticleBody extends StatefulWidget {
 }
 
 class ArticleBodyState extends State<ArticleBody> {
+  final RegExp _urlRegex = RegExp(r'https?://[\w\-.]+\.[\w/\-.?&=%+]+');
+
   @override
   Widget build(BuildContext context) {
     final TextStyle textStyle = Theme.of(context).textTheme.body1.copyWith(height: 1.15);
@@ -23,16 +25,27 @@ class ArticleBodyState extends State<ArticleBody> {
     );
   }
 
+  /// Grey out quoted text
   List<TextSpan> _buildTextSpans(BuildContext context) {
-    // TODO quoted text, selection, ...
+    final TextStyle quoteStyle =
+        Theme.of(context).textTheme.body1.copyWith(color: Colors.grey.shade400);
+    final List<String> lines = widget.text.split('\n');
+    return lines.map<TextSpan>((line) {
+      TextSpan(
+        style: line.startsWith('>') ? quoteStyle : null,
+        children: _findUrls('$line\n', context),
+      );
+    }).toList();
+  }
+
+  /// Create clickable URLs
+  List<TextSpan> _findUrls(String text, BuildContext context) {
     List<TextSpan> textSpans = [];
     Match prevMatch;
-    // URI regex
-    RegExp regex = RegExp(r'https?://[\w\-.]+\.[\w/\-.?&=%+]+');
-    for (Match match in regex.allMatches(widget.text)) {
-      // Add raw text until match
+    for (Match match in _urlRegex.allMatches(text)) {
+      // Add text before until match
       int textStart = prevMatch?.end ?? 0;
-      textSpans.add(TextSpan(text: widget.text.substring(textStart, match.start)));
+      textSpans.add(TextSpan(text: text.substring(textStart, match.start)));
       // Add clickable uri
       String uri = match.group(0);
       textSpans.add(LinkTextSpan(
@@ -42,10 +55,10 @@ class ArticleBodyState extends State<ArticleBody> {
       ));
       prevMatch = match;
     }
-    // Add raw text after last match
+    // Add text after last match
     int textStart = prevMatch?.end ?? 0;
-    if (textStart <= widget.text.length) {
-      textSpans.add(TextSpan(text: widget.text.substring(textStart, widget.text.length)));
+    if (textStart <= text.length) {
+      textSpans.add(TextSpan(text: text.substring(textStart, text.length)));
     }
     return textSpans;
   }
